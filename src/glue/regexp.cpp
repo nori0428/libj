1f4033c3ec388067afa25ba13b5c4a7d72c1de84
nori0428 <nori.0428@gmail.com>
2012-11-15 13:18:34 +0900

    enable to use pcre16 instead of iv::aero

--- src/glue/regexp.cpp
+++ src/glue/regexp.cpp
@@ -1,7 +1,12 @@
 // Copyright (c) 2012 Plenluno All rights reserved.
 
-#include <iv/aero/aero.h>
+#ifdef LIBJ_USE_IV_AERO
+# include <iv/aero/aero.h>
+#else
+# include <pcre.h>
+#endif
 #include <vector>
+#include <iostream>
 
 #include "./regexp.h"
 
@@ -10,11 +15,21 @@ namespace glue {
 
 RegExp* RegExp::create(const U16String& pattern, unsigned int flags) {
     RegExp* re = new RegExp(pattern, flags);
+    int error = 0;
+
+#ifdef LIBJ_USE_IV_AERO
     iv::core::Space space;
     int fs = (flags & IGNORE_CASE ? iv::aero::IGNORE_CASE : 0)
            | (flags & MULTILINE ? iv::aero::MULTILINE : 0);
-    int error = 0;
     re->code_ = iv::aero::Compile(&space, pattern, fs, &error);
+#else
+    const char* errstr;
+
+    int fs = (flags & IGNORE_CASE ? PCRE_CASELESS : 0)
+           | (flags & MULTILINE ? PCRE_MULTILINE : 0);
+    re->code_ = pcre16_compile(pattern.c_str(), fs, &errstr, &error, NULL);
+#endif
+
     if (!error && re->code_) {
         return re;
     } else {
@@ -24,7 +39,12 @@ RegExp* RegExp::create(const U16String& pattern, unsigned int flags) {
 }
 
 RegExp::~RegExp() {
+#ifdef LIBJ_USE_IV_AERO
     delete static_cast<iv::aero::Code*>(code_);
+#else
+    free(static_cast<pcre16*>(code_));
+#endif
+
 }
 
 bool RegExp::global() const {
@@ -43,6 +63,8 @@ bool RegExp::execute(
     const U16String& str,
     int offset,
     std::vector<int>& captures) const {
+
+#ifdef  LIBJ_USE_IV_AERO
     static iv::aero::VM vm;
     iv::aero::Code* code = static_cast<iv::aero::Code*>(code_);
     captures.clear();
@@ -51,6 +73,23 @@ bool RegExp::execute(
         captures.push_back(-1);
     int res = vm.Execute(code, str, captures.data(), offset);
     return res == iv::aero::AERO_SUCCESS;
+#else
+
+    pcre16* code = static_cast<pcre16*>(code_);
+    captures.clear();
+
+// be same value as iv::aero::Code*->captures() * 2
+#define	DEFAULT_CAPTURE_CAPACITY	(4)
+    captures.reserve(DEFAULT_CAPTURE_CAPACITY * 3);
+#undef	DEFAULT_CAPTURE_CAPACITY
+
+    size_t n = captures.capacity();
+    for (size_t i = 0; i < n; i++)
+        captures.push_back(-1);
+    int res = pcre16_exec(code, NULL, str.c_str(), str.length(),
+                          offset, 0, captures.data(), n);
+    return (res > 0);
+#endif
 }
 
 }  // namespace glue
